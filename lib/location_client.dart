d4339da2ae1d54cbf23168226c10fe19921d9743
matheus.geiser <matheusgeiser@gmail.com>
2021-11-19 16:22:38 -0300

    removing comments

--- lib/location_client.dart
+++ lib/location_client.dart
@@ -23,12 +23,8 @@ class GeoLocationClient implements LocationClient {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return GeoLocationPermissionEnum.serviceNotAvailable;
     }
 
@@ -36,17 +32,11 @@ class GeoLocationClient implements LocationClient {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return GeoLocationPermissionEnum.denied;
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
       return GeoLocationPermissionEnum.deniedForever;
     }
     return GeoLocationPermissionEnum.allowed;
